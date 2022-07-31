a980b23ccc3d26d53df3b1e0f634b11d6ffab721
Djellza95 <djellza@origin3agency.com>
2022-07-31 20:57:29 +0200

    Created project

---
+++ RandomUsers/RandomUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUsers
+//
+//  Created by Djellza Rrustemi on 31.7.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
